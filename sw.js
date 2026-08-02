72e94d1b84fc423e2fce6e5023bc1a49577cecc8
Brandon E <ilevanshine@gmail.com>
2026-08-02 19:13:02 -0400

    Refresh mobile editor cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'frontseat-commercial-v3';
+const CACHE = 'frontseat-commercial-v4';
 const ASSETS = ['./','index.html','styles.css','commercial.css','layout-editor.css','app.js','commercial.js','layout-editor.js','manifest.webmanifest','privacy.html','terms.html'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(()=>self.clients.claim())));
