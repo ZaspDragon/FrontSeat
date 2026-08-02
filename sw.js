bf94273c3adbe983b32c463f30c71098cebd4e9c
Brandon E <ilevanshine@gmail.com>
2026-08-02 19:42:20 -0400

    Refresh stable cache for safe editor release

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'frontseat-commercial-v5';
+const CACHE = 'frontseat-commercial-v6';
 const ASSETS = ['./','index.html','styles.css','commercial.css','layout-editor.css','layout-mobile-controls.css','app.js','commercial.js','layout-editor.js','layout-mobile-controls.js','manifest.webmanifest','privacy.html','terms.html'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(()=>self.clients.claim())));
